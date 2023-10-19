168fa9b72cce9d9311192a1703c5626929e8a6da
WillSevero <severo@tuta.io>
2023-10-19 13:58:35 -0300

    criado model anuncio

--- anuncios/models.py
+++ anuncios/models.py
@@ -8,17 +8,17 @@ class Categoria(models.Model):
 #     class Meta:
 #          ordering = ['titulo']
 
-# class Anuncio(models.Model):
-#     titulo = models.CharField(max_length=30)
-#     descricao = models.TextField(null=True, blank=True)
-#     preco = models.DecimalField(max_digits=11, decimal_places=2)
-#     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-#     criado_em = models.DateTimeField(auto_now_add=True)
-#     auterado_em = models.DateTimeField(auto_now=True)
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=30)
+    descricao = models.TextField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=11, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    auterado_em = models.DateTimeField(auto_now=True)
 
 
-#     def __str__(self):
-#             return self.titulo
+    def __str__(self):
+            return self.titulo
     
 #     class Meta:
 #          ordering = ['-id']
