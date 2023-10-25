9d21b8b28cd7ac1cf42a7d4afa179eb2a975e47c
WillSevero <severo@tuta.io>
2023-10-24 23:50:48 -0300

    pagina de categoria

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from anuncios import models
+
+
+
 from .models import Categoria
 from .models import Anuncio
 
@@ -10,3 +13,12 @@ def home(request):
     ultimos_anuncios = Anuncio.objects.all()[:12]
 
     return render(request, 'home.html', {'categorias': categorias, 'anuncios':ultimos_anuncios})
+
+def categoria(request, categoria_id ):
+    categoria = get_object_or_404(Categoria, id=categoria_id)
+    
+    categorias = Categoria.objects.all()
+
+    anuncios = Anuncio.objects.filter(categoria=categoria)
+
+    return render(request, 'home.html', {'categorias': categorias, 'anuncios':anuncios, 'categoria':categoria})
